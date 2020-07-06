93a765b16b6fda7341c75b27120bc5548bd2cf63
virginiayjd7 <37305214+virginiayjd7@users.noreply.github.com>
2020-07-05 21:44:42 -0500

    uses-permission

--- app/src/main/java/com/desarrollo/aquino_asynctaskhttpurlconnection/MainActivity.java
+++ app/src/main/java/com/desarrollo/aquino_asynctaskhttpurlconnection/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements InterfaceFragment
         }
         imputLibro = (EditText) findViewById(R.id.ingresoLibro);
         textoTitle = (TextView) findViewById(R.id.title);
-       // textoAutor = (TextView) findViewById(R.id.li);
+        textoAutor = (TextView) findViewById(R.id.autorLibro);
     }
 
     public void buscarLibro(View view) {
